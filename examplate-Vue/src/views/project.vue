e53319adb4a65a775f7b2228d2cec58a92597341
lzj222312 <645383348@qq.com>
2017-10-09 02:04:12 +0800

    新增css和组件

--- examplate-Vue/src/views/project.vue
+++ examplate-Vue/src/views/project.vue
@@ -1,15 +1,22 @@
 <template>
-	<div>
-		我是项目
-	</div>
+  <div class="project">
+    <div class="add-project-box">
+      <i class='fa fa-plus-square'></i>
+      <span>创建项目</span>
+    </div>
+    <div class="block-content">
+      这是工作台页面
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-	}
-</script>
 
+  export default {
+    name: 'Project'
+  }
+</script>
 <style>
 
- 
 </style>
+
--- examplate-Vue/src/views/work.vue
+++ examplate-Vue/src/views/work.vue
@@ -1,15 +1,20 @@
 <template>
-	<div>
-		我是工作台
-	</div>
+  <div>
+    <div class="block-content">
+      这是工作台页面
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-	}
+  export default {
+    name: 'Code',
+    data () {
+      return {}
+    }
+  }
 </script>
-
 <style>
 
- 
 </style>
+
